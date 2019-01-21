8139e71dc2395797d14b4f2dc051de9a7d3349d0
billy <billyliadphillips@gmail.com>
2019-01-21 18:30:38 +0000

    added in styles and thumbnail

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,13 +1,41 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import Card from "./Card";
+import CardSection from "./CardSection";
 
-const AlbumDetail = props => {
+const AlbumDetail = ({ album }) => {
+  // destructure album from props
+  const { title, artist, thumbnail_image } = album;
+  // destructure items from album
+  const { thumbnailStyle, headerContentStyle } = styles;
+  //destructure styles
   return (
-    <View>
-      <Card>{props.album.title}</Card>
-    </View>
+    <Card>
+      <CardSection>
+        <View>
+          <Image
+            style={thumbnailStyle}
+            source={{ uri: thumbnail_image }}
+          />
+        </View>
+        <View style={headerContentStyle}>
+          <Text>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
+      </CardSection>
+    </Card>
   );
+};
+
+const styles = {
+  headerContentStyle: {
+    flexDirection: "column",
+    justifyContent: "space-around"
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  }
 };
 
 export default AlbumDetail;

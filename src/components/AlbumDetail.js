1ee2e3dc89c883338af0ad10ac38a2adb9e843fc
billy <billyliadphillips@gmail.com>
2019-01-21 17:28:11 +0000

    Added in card component and props.children

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React from "react";
+import { View, Text } from "react-native";
+import Card from "./Card";
 
 const AlbumDetail = props => {
-    return (
-            <Text>{props.album.title}</Text>
-    )
+  return (
+    <View>
+      <Card>{props.album.title}</Card>
+    </View>
+  );
 };
 
 export default AlbumDetail;

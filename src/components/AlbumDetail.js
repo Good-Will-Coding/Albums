5a42efa52dd80e43e71dad011ac95e5107c4503c
billy <billyliadphillips@gmail.com>
2019-01-22 10:08:07 +0000

    button url redirects to buy

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,36 +1,39 @@
 import React from "react";
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, Linking } from "react-native";
 import Card from "./Card";
 import CardSection from "./CardSection";
 import Button from "./Button";
 
 const AlbumDetail = ({ album }) => {
   // destructure album from props
-  const { title, artist, thumbnail_image, image } = album;
+  const { title, artist, thumbnail_image, image, url } = album;
   // destructure items from album
-  const { thumbnailStyle, headerContentStyle, thumbnailContainerStyle, headerTextStyle, imageStyle } = styles;
+  const {
+    thumbnailStyle,
+    headerContentStyle,
+    thumbnailContainerStyle,
+    headerTextStyle,
+    imageStyle
+  } = styles;
   //destructure styles
   return (
     <Card>
       <CardSection>
         <View style={thumbnailContainerStyle}>
-          <Image
-            style={thumbnailStyle}
-            source={{ uri: thumbnail_image }}
-          />
+          <Image style={thumbnailStyle} source={{ uri: thumbnail_image }} />
         </View>
         <View style={headerContentStyle}>
           <Text style={headerTextStyle}>{title}</Text>
           <Text>{artist}</Text>
         </View>
       </CardSection>
-     
+
       <CardSection>
-          <Image style={imageStyle} source={{ uri: image }} />
+        <Image style={imageStyle} source={{ uri: image }} />
       </CardSection>
-      
+
       <CardSection>
-          <Button />
+        <Button onPress={() => Linking.openURL(url)}>Buy Now</Button>
       </CardSection>
     </Card>
   );
@@ -49,15 +52,15 @@ const styles = {
     width: 50
   },
   thumbnailContainerStyle: {
-      justifyContent: 'center',
-      alignItems: 'center',
-      marginLeft: 10,
-      marginRight: 10
+    justifyContent: "center",
+    alignItems: "center",
+    marginLeft: 10,
+    marginRight: 10
   },
   imageStyle: {
-      height: 300,
-      flex: 1,
-      width: null
+    height: 300,
+    flex: 1,
+    width: null
   }
 };
 
